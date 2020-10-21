646d2001a9b18571a1f7f57a703bc3b50b8a1518
shawn <yu.zhang@jian24.com>
2020-10-21 14:19:17 +0800

    update 1.0.0

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 
 setup(
     name='qx-data-storage',
-    version='1.0.9',
+    version='1.0.0',
     author='Shawn',
     author_email='q-x64@live.com',
     url='https://github.com/qx-oo/qx-data-storage/',
-    description='Django base apps.',
+    description='Django data storage apps.',
     long_description=open("README.md").read(),
     packages=find_packages(exclude=["tests", "qx_test"]),
     install_requires=[
