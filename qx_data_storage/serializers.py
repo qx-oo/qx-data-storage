9ae0c1f45ae5468bbedb34d8face308dcb733659
shawn <yu.zhang@jian24.com>
2021-03-04 15:54:19 +0800

    修改图片时间戳为16进制数，减少长度

--- qx_data_storage/serializers.py
+++ qx_data_storage/serializers.py
@@ -44,8 +44,9 @@ class OssImageSerializerMixin():
         return url
 
     def get_upload_url(self, location, unique_id):
+        ts = hex(int(time.time() * 10000000))[2:]
         obj_name = "upload/{}/{}-{}".format(
-            location, int(time.time() * 1000), unique_id)
+            location, ts, unique_id)
         try:
             ins = AutoOssStorage()
             upload_url = ins.sign_url('PUT', obj_name)
